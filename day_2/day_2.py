72b9ec56c72720dde4b22c85c19a8b5ae839c075
Victor Penelski <victor.penelski@gmail.com>
2020-12-02 15:24:47 +0200

    extract generator method
    fix is_valid_idx self.ref

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -22,22 +22,26 @@ class Validation:
         idx_two = self.max - 1
         if len(self.password) <= idx_two:
             return False
-        if self.password[idx_one] == char and self.password[idx_two] != char:
+        if self.password[idx_one] == self.char and self.password[idx_two] != self.char:
             return True
-        if self.password[idx_one] != char and self.password[idx_two] == char:
+        if self.password[idx_one] != self.char and self.password[idx_two] == self.char:
             return True
         return False
+
+
+def validation_from_raw_string(string):
+    split = string.split(" ")
+    min_occurrences = int(split[0].split("-")[0])
+    max_occurrences = int(split[0].split("-")[1])
+    char = split[1].strip(":")
+    password = split[2]
+    return Validation(min_occurrences, max_occurrences, char, password)
 
 
 records = file_lines_as_list("day_2_input.txt")
 valid_passwords = 0
 for el in records:
-    split = el.split(" ")
-    min_occurrences = int(split[0].split("-")[0])
-    max_occurrences = int(split[0].split("-")[1])
-    char = split[1].strip(":")
-    password = split[2]
-    validation = Validation(min_occurrences, max_occurrences, char, password)
+    validation = validation_from_raw_string(el)
     if validation.is_valid_idx():
         valid_passwords += 1
 
